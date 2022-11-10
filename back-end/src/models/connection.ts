ac897ebc81ae75ec7f47a3df5619878b942cf0a3
Felipe <felmartins1985@gmail.com>
2022-11-10 12:00:29 -0300

    feat: inclui o db na nuvem

--- back-end/src/models/connection.ts
+++ back-end/src/models/connection.ts
@@ -1,8 +1,14 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
-const MONGO_DB_URL = 'mongodb://0.0.0.0:27017/produtos';
+const {
+  MONGOHOST,
+  MONGOPASSWORD,
+  MONGOPORT,
+  MONGOUSER,
+} = process.env;
 
+const MONGO_DB_URL = `mongodb://${MONGOUSER}:${MONGOPASSWORD}@${MONGOHOST}:${MONGOPORT}`;
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI
     || MONGO_DB_URL,
